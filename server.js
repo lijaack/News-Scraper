e2333722b9e8ce8fa851a1fabd5045d3925ce5dd
Jack <lijaack@yahoo.com>
2019-01-21 18:34:48 -0800

    added npm packets

--- server.js
+++ server.js
@@ -5,24 +5,37 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
-
-// Require all models
-var db = require("./models");
-
 var PORT = 3000;
 
-// Initialize Express
 var app = express();
-
-// Configure middleware
 
 // Use morgan logger for logging requests
 app.use(logger("dev"));
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+
 // Make public a static folder
 app.use(express.static("public"));
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//sets up app to use body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//view engine
+app.engine("handlebars", exphbs({ defaultLayout: "main", helpers: require('handlebars-helpers') }));
+app.set("view engine", "handlebars");
+
+
+
+// Routes
+// =============================================================
+require("./controllers/scraper-routes.js")(app);
+
+
+// Require all models
+var db = require("./models");
 
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/", { useNewUrlParser: true });
