87cfb7abe04032193f84e3b950816be03162ecd9
Jack <lijaack@yahoo.com>
2019-01-22 20:39:00 -0800

    fixed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
